23f33db0b75f13c49112ac2c33734cab3826f022
Alejandrodlk <ale_165851@hotmail.com>
2022-09-11 16:00:01 -0300

    modelos y consultas done

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false })); // para recibir parametro a traves del req.body
 
 app.use('/', indexRouter);
 app.use(moviesRoutes);
